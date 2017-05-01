053c30de38cc5a550ab7de6b0e9e64dfeeeedc1b
Theodore R. Smith <theodore@phpexperts.pro>
2017-05-01 15:33:44 +0530

    Fixed the license headers.

--- src/Money.php
+++ src/Money.php
@@ -1,13 +1,11 @@
 <?php
-/**
-This file is a part of the MoneyType library, a PHPExperts.pro Project.
-
-Copyright (c) 2017 PHP Experts, Inc. <www.phpexperts.pro>
-Authored by Theodore R. Smith <theodore@phpexperts.pro>
- * 2017-05-01 14:17 IST
-
-This file is licensed under the terms of the MIT license:
-**/
+// This file is a part of the MoneyType library, a PHPExperts.pro Project.
+//
+// Copyright (c) 2017 PHP Experts, Inc. <www.phpexperts.pro>
+// Authored by Theodore R. Smith <theodore@phpexperts.pro>
+//  * 2017-05-01 14:17 IST
+//
+// This file is licensed under the terms of the MIT license:
 
 namespace PHPExperts\MoneyType;
 
